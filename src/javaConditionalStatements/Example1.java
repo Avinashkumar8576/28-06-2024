4e097656f25cf2e21db6128c28941bf2bc3d382b
Avinashkumar8576 <avinashkumar90089008@gmail.com>
2024-06-28 14:54:07 +0530

    first commit

---
+++ src/javaConditionalStatements/Example1.java
@@ -0,0 +1,20 @@
+package javaConditionalStatements;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		
+		int studentAge = 10;
+		
+		boolean condition = (studentAge >= 18);
+		
+		System.out.println(condition);
+		
+		if(condition)
+		{
+			System.out.println("the person is major");
+		}
+		
+	}
+
+}
